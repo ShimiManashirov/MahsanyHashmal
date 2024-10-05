ebb9ade2db08fca46b1a29ede2a36e58aead69d2
ShimiManashirov <122288727+ShimiManashirov@users.noreply.github.com>
2024-10-05 18:51:19 +0300

    make DB class, add get hash

--- DBconnect/db.js
+++ DBconnect/db.js
@@ -1,34 +1,66 @@
-const express = require('express');
-const { del } = require('express/lib/application');
-const { MongoClient } = require('mongodb');
-require('dotenv').config();
+import { MongoClient } from 'mongodb';
 
-const app = express();
-const client = new MongoClient('mongodb+srv://shimitamiroz:shimitamiroz@shopdb.hqf0u.mongodb.net/');
-const database = client.db('ShopDB');
 
-//connect to DB
-async function connectToMongoDB() {
-  await client.connect();
-    console.log("Connected successfully to MongoDB");
-}
-//creating a new collection
-async function new_collection(name) {
-  connectToMongoDB()
-  console.log('start creating collection')
-  collection = database.createCollection(name)
-  console.log('collection created' + 'name' + name)
-  client.close
+class DB {
+  constructor() {
+    this.client = new MongoClient('mongodb+srv://shimitamiroz:shimitamiroz@shopdb.hqf0u.mongodb.net/');
+    this.database = this.client.db('ShopDB');
+  }
+
+  async connect() {
+    try {
+      await this.client.connect();
+      console.log("Connected successfully to MongoDB");
+    } catch (error) {
+      console.error("Failed to connect to MongoDB", error);
+    }
+  }
+
+  async new_collection(name) {
+    console.log('Start creating collection');
+    try {
+      const collection = await this.database.createCollection(name);
+      console.log(`Collection created: ${name}`);
+      return collection;
+    } catch (error) {
+      console.error(`Failed to create collection ${name}`, error);
+    }
+  }
+  async add_doc(item_json, collection_name) {
+    console.log('Start adding document');
+    try {
+      const collection = this.database.collection(collection_name);
+      const result = await collection.insertOne(item_json);
+      console.log('Finished adding document', result.insertedId);
+      return result;
+    } catch (error) {
+      console.error('Failed to add document', error);
+      throw error;
+    }
+  }
+  async get_hash_from_user(user_name, collection_name){
+    try{
+      const document = await this.database.collection(collection_name).findOne({username : user_name})
+      if (!document) {
+        console.log(`User ${user_name} not found`);
+        return null;
+      }
+      return document.hash;
+    }catch(error){
+      console.error('Failed to find document', error);
+      throw error;
+    }
+  }
+  close() {
+    this.client.close();
+    console.log("Connection to MongoDB closed");
+  }
 }
 
-//add one document to collection
-async function add_doc(item_jason,collection_name) {
-  connectToMongoDB()
-  console.log('start adding document')
-  database.collection(collection_name).insertOne(item_jason)
-  console.log('finish adding document')
-  client.close
-}
+
+
+
+
 
 //delete user
 async function delete_user(user_name,collection_name) {
@@ -44,4 +76,6 @@ async function update_password(user_name, new_password,collection_name) {
   client.close
 }
 
-
+const db= new DB
+const pass = await db.get_password_from_user("oz","test")
+console.log(pass)
